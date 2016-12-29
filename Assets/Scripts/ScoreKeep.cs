a3864f5674637f4be04b40421ebac6a084a4fc1b
Geoff Whitehead <geoff1012@gmail.com>
2016-12-29 22:46:11 +0000

    keeping score of box collisions

---
+++ Assets/Scripts/ScoreKeep.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeep : MonoBehaviour {
+
+    private int score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        score += 1;
+        print(score);
+    }
+
+}
